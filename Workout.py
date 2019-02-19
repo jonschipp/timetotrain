5a0d60cea397c72c86cd3f8e43fa8a47d1bc9518
Jon Schipp <jonschipp@gmail.com>
2019-02-18 18:49:19 -0600

    Reorg

--- Workout.py
+++ Workout.py
@@ -12,6 +12,7 @@ class Workout:
     self.frequency = frequency # How many days per week
     self.slots = slots     # How many slots per day
 
+
   def generate_weeks(self, weeks: int) -> list:
       if not weeks:
           # Set default
@@ -27,21 +28,11 @@ class Workout:
 
       return self.wb.sheetnames
 
+
   def generate_frequency(self, frequency: int) -> int:
       if not frequency:
           # Set default
           frequency = self.frequency
-
-      # Set style
-      font = Font(
-          name='Calibri', size=48, bold=True, color=colors.WHITE
-      )
-      fill = PatternFill(
-          fill_type='solid', bgColor=colors.BLACK,
-      )
-      alignment = Alignment(
-          horizontal="center", vertical="center"
-      )
 
       for sheet in self.wb.sheetnames:
           # Get sheet
@@ -49,13 +40,13 @@ class Workout:
           begin_row = 4 # We start in the 4th row
           begin_col = 4 # We start in the 4th column i.e. D
           for day in range(1, frequency + 1):
-              print(day)
               # Add day header e.g .[ Day 1 ] [ Day 2 ] [ Day 3 ]
-              cws = self.wb[sheet]
-              cws.column_dimensions[get_column_letter(begin_col)].width = 60
-              cws.cell(row=begin_row, column=begin_col, value=f"Day {day}")
-              cws.fill = fill
-              cws.alignment = alignment
+              currentSheet = self.wb[sheet]
+              currentCell = currentSheet.cell(
+                  row=begin_row, column=begin_col, value=f"Day {day}"
+              )
+              self.test('Blah')
+              self.set_style(currentSheet, currentCell, begin_col, colors.BLACK, 48, 'Helvetica')
               begin_col = begin_col + 2
 
       return frequency
@@ -66,7 +57,26 @@ class Workout:
           # Set default
           slots = self.slots
 
+      for sheet in self.wb.sheetnames:
+          # Get sheet
+          # Generate tables for days in sheet
+          begin_row = 6 # We start in the 4th row
+          begin_col = 4 # We start in the 4th column i.e. D
+          for slot in range(1, slots + 1):
+              # Add exercise slots e.g.
+              # [ Exercise 1 ]
+              # [ Exercise 2 ]
+              # [ Exercise 3 ]
+              currentSheet = self.wb[sheet]
+              currentCell = currentSheet.cell(
+                  row=begin_row, column=begin_col, value=f"Exercise {slot}"
+              )
+              self.test('Blah')
+              self.set_style(currentSheet, currentCell, begin_col, colors.BLACK, 32, 'Helvetica')
+              begin_col = begin_col + 2
+
       return slots
+
 
   def save(self, filename: str) -> str:
       if not filename:
@@ -75,3 +85,27 @@ class Workout:
 
       self.wb.save(filename)
       return filename
+
+
+def set_style(self, sheet: object, cell: object, col: int, color: str, size: int, font: str) -> object:
+      # Set style
+      font = Font(
+          name=font, size=size, bold=True, color=colors.WHITE
+      )
+      fill = PatternFill(
+          fill_type='solid', bgColor=color,
+      )
+      alignment = Alignment(
+          horizontal="center", vertical="center"
+      )
+
+      sheet.column_dimensions[get_column_letter(col)].width = 60
+      cell.font = font
+      cell.fill = fill
+      cell.alignment = alignment
+      return cell
+
+def test(self, msg: str) -> str:
+    if not msg:
+        msg = 'Test'
+    return msg
